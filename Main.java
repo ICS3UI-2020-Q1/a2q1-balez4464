5d6d4ad3db1e4df90c787ccd2d02e96f8bce6174
balez4464 <balez4464@wrdsb.ca>
2020-09-15 13:50:27 +0000

    Made a program that tells user if their number is even or odd

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *This program calculates if the users number is even or odd
+ * @author Zachary Balean
  */
 public class Main {
 
@@ -9,7 +10,25 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Creates new Scanner
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Please enter an integer:");
+
+    //This is the number the user inputs
+    int usersNumber = input.nextInt();
+
+    //divides users number by 2 and also remainder
+    int dividedNumber = (usersNumber % 2);
+  
+
+    //Checks to see if number is even or odd
+    if(dividedNumber >= 1){
+      System.out.println(usersNumber + " is an odd number");
+    } else {
+      System.out.println(usersNumber + " is an even number");
+    }
+    
     
   }
 }
